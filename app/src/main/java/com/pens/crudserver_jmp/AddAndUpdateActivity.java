a0ddc6b9e1be07f1c371ee8255efa49ee90ba139
abdaziz-04 <abdaziz1713@gmail.com>
2023-07-24 03:21:07 +0700

    Logika Add and Update Activity

---
+++ app/src/main/java/com/pens/crudserver_jmp/AddAndUpdateActivity.java
@@ -0,0 +1,14 @@
+package com.pens.crudserver_jmp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddAndUpdateActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_and_update);
+    }
+}
